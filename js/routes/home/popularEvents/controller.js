3ea5b8a6bd29236bebc590dd29694ef4d3389059
devtoni <devtonirm@gmail.com>
2017-07-30 17:15:39 +0200

    adapted controllers to standard js style

--- js/routes/home/popularEvents/controller.js
+++ js/routes/home/popularEvents/controller.js
@@ -1,18 +1,18 @@
 /* global angular */
 (function () {
-  angular.module('skyEventApp')
-  .controller('popularEvents', function ($scope, eventBriteDataService) {
+  'use strict'
+  var app = angular.module('skyEventApp')
+  app.controller('popularEvents', popularEvents)
+  function popularEvents (eventBriteDataService) {
+    var self = this
     navigator.geolocation.getCurrentPosition(function (position) {
       var latitude = position.coords.latitude
       var longitude = position.coords.longitude
       eventBriteDataService.mostPopular(latitude, longitude)
         .then(function (data) {
-          $scope.showEvent = true
-          $scope.eventsArray = data.data.events.splice(0, 8)
-        })
-        .catch(function (error) {
-          $scope.showEvent = false
+          self.showEvent = true
+          self.eventsArray = data.data.events.splice(0, 8)
         })
     })
-  })
+  }
 })()
--- js/routes/home/searchController/controller.js
+++ js/routes/home/searchController/controller.js
@@ -1,22 +1,25 @@
 /* global angular */
-angular.module('skyEventApp')
-  .controller('searchController', function ($scope, $location, auxiliarServices, eventBriteDataService) {
+
+(function () {
+  'use strict'
+  var app = angular.module('skyEventApp')
+  app.controller('searchController', searchController)
+  function searchController ($location, auxiliarServices, eventBriteDataService) {
+    var self = this
     var daySelected = ''
     var distanceSelected = ''
-
-    $scope.getDay = function () {
-      daySelected = $scope.daySelected
+    self.getDay = function () {
+      daySelected = self.daySelected
     }
-
-    $scope.getDistance = function () {
-      distanceSelected = $scope.distanceSelected
+    self.getDistance = function () {
+      distanceSelected = self.distanceSelected
     }
-    $scope.getQuery = function () {
-      var eventName = $scope.eventName
-      var locationName = $scope.locationName
+    self.getQuery = function () {
+      var eventName = self.eventName
+      var locationName = self.locationName
       var data = { eventName: eventName, locationName: locationName, daySelected: daySelected, distanceSelected: distanceSelected }
       auxiliarServices.setDataInput(data)
       $location.path('/search-results/')
     }
-    $scope.imgNoAv = 'http://res.cloudinary.com/drenzfi8m/image/upload/v1501183394/no-image-available_yxkp4d.png'
-  })
+  }
+})()
