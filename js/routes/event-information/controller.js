055a320c1aa155360264b6d5869b996b5cd8ff65
devtoni <devtonirm@gmail.com>
2017-07-30 18:33:56 +0200

    modified readme

--- js/routes/event-information/controller.js
+++ js/routes/event-information/controller.js
@@ -1,22 +1,20 @@
-(function () {
   /* global angular */
-  angular.module('skyEventApp')
-    .controller('eventInformation', function ($scope, $sce, $routeParams, eventBriteDataService, auxiliarServices) {
-      var ids = $routeParams.id.split('&')
-      var [eventId, venueId] = [ids[0], ids[1]]
-
-      eventBriteDataService.getLocalization(venueId)
-        .then(function (data) {
-          $scope.locaInfo = data
-        })
-
-      eventBriteDataService.getId(eventId)
-        .then(function (data) {
-          $scope.infoEvent = data
-        })
-      $scope.goBack = function () {
-        window.history.back()
-      }
-      $scope.renderHtml = auxiliarServices.renderHtmlWithAngular
-    })
+(function () {
+  'use strict'
+  var app = angular.module('skyEventApp')
+  app.controller('eventInformation', eventInformation)
+  function eventInformation ($sce, $routeParams, eventBriteDataService, auxiliarServices) {
+    var self = this
+    var ids = $routeParams.id.split('&')
+    var [eventId, venueId] = [ids[0], ids[1]]
+    self.renderHtml = auxiliarServices.renderHtmlWithAngular
+    eventBriteDataService.getLocalization(venueId)
+      .then(function (data) {
+        self.locaInfo = data
+      })
+    eventBriteDataService.getId(eventId)
+      .then(function (data) {
+        self.infoEvent = data
+      })
+  }
 })()
